660a94291f998011a24f41d53214aa94056abe77
Letder40 <adrian04rv@gmail.com>
2023-05-17 15:47:08 +0200

    additions

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ $(whoami) != "root" ]]; then
+  echo "[!] You need to be root to install whysp"
+  exit 1
+fi
+pip install ./requirements.txt
+mkdir /opt/whysp
+chmod 777 /opt/whysp
+cp whysp.py /opt/whysp/whysp.py
+ln -s /opt/whysp/whysp.py /usr/bin/whysp
--- whysp.py
+++ whysp.py
@@ -1,34 +1,33 @@
+#!/usr/bin/python3
 import pytube
-import whisper
 import sys
 from os import remove
-
-#### PROVISIONAL CODE FOR NUMBA DEPRECATION WARNING
-from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
-import warnings
-
-warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
-warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
-#### ----------------------------------------------
-
-model = whisper.load_model("small")
 
 try:
     link = sys.argv[1]
 except: 
-    sys.stderr.write("A link must be passed as positional argument\n")
+    sys.stderr.write(" [!] A link must be passed as positional argument\n")
     sys.exit(1)
+
+import whisper
+
+try:
+    model = sys.argv[2]
+except: 
+    model = whisper.load_model("base")
 
 try:
     video = pytube.YouTube(link)
 except:
-    sys.stderr.write("The video does not exists or it is not posible to download it\n")
+    sys.stderr.write(" [!] The video does not exists or it is not posible to download it\n")
     sys.exit(1)
 
 video = video.streams.get_audio_only()
 video.download(filename=".toRead.mp4")
+print("video download complete...")
 
-
+print(" [?] Transcribing it...")
 result = model.transcribe(".toRead.mp4")
 remove(".toRead.mp4")
-print(result["text"])
+open("transcription.txt", x).write(result["text"])
+print("\n [+] Transcripttion complete ")
